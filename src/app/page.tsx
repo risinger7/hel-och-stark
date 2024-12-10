beb72976285744ff64af64b3af5e86695430f1fd
risinger7 <viktorrisinger@gmail.com>
2024-12-10 17:37:34 +0100

    popular treatments idea

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,7 @@ import { lexend200, lexend400, lexend700 } from "@/components/fonts/fonts";
 import Footer from "@/components/footer/Footer";
 import Navbar from "@/components/navbar/Navbar";
 import { useRouter } from "next/navigation";
+import Popular from "@/components/popular/Popular";
 
 export default function Home() {
   const router = useRouter();
@@ -13,30 +14,33 @@ export default function Home() {
   };
 
   return (
-    <div className="home-container">
-      <Navbar />
-      <Container>
-        <div className="welcome-page">
-          <div className="welcome-image-container">
-            {/* <img className="welcome-image" src="/hero2.jpg"></img> */}
-            <div className="welcome-container">
-              <h1 className={`${lexend700.className} welcome-title`}>
-                Varmt välkommen till Hel & Stark!
-              </h1>
-              <p className={`${lexend400.className} welcome-text`}>
-                Välkommen till en plats där din hälsa och välmående står i
-                centrum med behandlingar anpassade efter dina unika behov.
-              </p>
-              <button
-                className={`${lexend700.className} welcome-button`}
-                onClick={handleNavigate}
-              >
-                Boka behandling
-              </button>
+    <div>
+      <div className="home-container">
+        <Navbar />
+        <Container>
+          <div className="welcome-page">
+            <div className="welcome-image-container">
+              {/* <img className="welcome-image" src="/hero2.jpg"></img> */}
+              <div className="welcome-container">
+                <h1 className={`${lexend700.className} welcome-title`}>
+                  Varmt välkommen till Hel & Stark!
+                </h1>
+                <p className={`${lexend400.className} welcome-text`}>
+                  Välkommen till en plats där din hälsa och välmående står i
+                  centrum med behandlingar anpassade efter dina unika behov.
+                </p>
+                <button
+                  className={`${lexend700.className} welcome-button`}
+                  onClick={handleNavigate}
+                >
+                  Boka behandling
+                </button>
+              </div>
             </div>
           </div>
-        </div>
-      </Container>
+        </Container>
+      </div>
+      <Popular />
       <Footer />
     </div>
   );
