31fe568dc72004270b249c055e8b10f7c2f0067d
risinger7 <viktorrisinger@gmail.com>
2025-03-25 19:20:14 +0100

    refresh

--- src/app/(pages)/behandlingar/page.tsx
+++ src/app/(pages)/behandlingar/page.tsx
@@ -1,4 +1,5 @@
 "use client";
+
 import Navbar from "@/components/navbar/Navbar";
 import "./behandlingar.css";
 /* import { lexend200, lexend700 } from "@/components/fonts/fonts"; */
@@ -17,58 +18,21 @@ export default function BehandlingarPage() {
   return (
     <div>
       <Navbar />
-      <h1>Titel</h1>
+      <div>
+        <h1>Titel</h1>
+      </div>
       <div className="grid-container">
         <div className="grid">
           <div className="grid-item">
-            <h1 className={` grid-item-title`}>
-              Behandlande/Terapeutisk Massage
-            </h1>
-            <div className="grid-item-text-cont"></div>
-            <p className={` grid-item-text`}>
-              För stela, korta, ömma, inflammerade muskler passar det bra med en
-              mjukgörande massage och spänningsbehandling i kombination med
-              djupverkande muskelbehandling och stretch.
+            <h1>rubrik</h1>
+            <p>pris</p>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Id a unde
+              veniam recusandae excepturi fugiat accusantium quos dolore fuga
+              voluptas, ab reprehenderit magnam cupiditate sunt deleniti
+              repellendus fugit ipsam corrupti.
             </p>
-            {!showText && (
-              <button
-                className={` text-button`}
-                onClick={() => setShowText(true)}
-              >
-                Läs mer
-              </button>
-            )}
-
-            {showText && (
-              <p className={`grid-text ${showText ? "show-text" : ""}`}>
-                En mycket effektiv behandlingsform för nacke, axlar och rygg,
-                tennis- mus- eller golfarmbåge, löparknä eller allmän
-                trötthetskänsla i ryggen. Om du har ett specifikt problem,
-                rekommenderar jag massage, 3-5 gånger med ungefär en veckas
-                mellanrum.
-                <span>
-                  <button
-                    className={` text-button`}
-                    onClick={() => setShowText(false)}
-                  >
-                    Visa mindre
-                  </button>
-                </span>
-              </p>
-            )}
-
-            <div
-              className={`grid-image-container ${showText ? "hideImage" : ""}`}
-            >
-              <img src="/hel1.png" className={`grid-image`}></img>
-            </div>
-            <div className="price-info">
-              <p>Pris: 100kr</p>
-
-              <button className={`grid-button boka`}>Boka</button>
-            </div>
           </div>
-
           <div className="grid-item">grid-item</div>
           <div className="grid-item">grid-item</div>
           <div className="grid-item">grid-item</div>
