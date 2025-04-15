053b969d70d91983c6688c85c453b08ee66f52dd
risinger7 <viktorrisinger@gmail.com>
2025-04-15 23:09:52 +0200

    fixing popular container

--- src/components/popular/Popular.tsx
+++ src/components/popular/Popular.tsx
@@ -3,6 +3,7 @@
 import { useState, useEffect } from "react";
 import "./popular.css";
 import { ChevronLeft, ChevronRight } from "lucide-react"; // Import arrow icons
+import { lexend300 } from "../fonts/fonts";
 
 const items = [
   "Varmt välkommen till Hel & Stark massageterapi...",
@@ -16,46 +17,45 @@ const items = [
 export default function Popular() {
   const [index, setIndex] = useState(0);
 
-  // Auto-scroll every 3s
+  // Auto-scroll every 8s
   useEffect(() => {
     const interval = setInterval(() => {
       nextSlide();
-    }, 9000);
+    }, 8000);
     return () => clearInterval(interval);
   }, [index]);
 
-  // Move to the next item
   const nextSlide = () => {
     setIndex((prevIndex) => (prevIndex + 1) % items.length);
   };
 
-  // Move to the previous item
   const prevSlide = () => {
     setIndex((prevIndex) => (prevIndex - 1 + items.length) % items.length);
   };
 
   return (
     <div className="popular">
-      <button className="arrow left" onClick={prevSlide}>
-        <ChevronLeft size={30} />
-      </button>
-
       <div className="carousel">
         <div
           className="carousel-inner"
-          style={{ transform: `translateX(-${index * 520}px)` }} // Adjust width dynamically
+          style={{ transform: `translateX(-${index * 520}px)` }}
         >
           {items.map((text, i) => (
-            <div className="item" key={i}>
+            <p className={`${lexend300.className} item`} key={i}>
               {text}
-            </div>
+            </p>
           ))}
         </div>
       </div>
 
-      <button className="arrow right" onClick={nextSlide}>
-        <ChevronRight size={30} />
-      </button>
+      <div className="arrows-container">
+        <button className="arrow" onClick={prevSlide}>
+          <ChevronLeft size={30} />
+        </button>
+        <button className="arrow" onClick={nextSlide}>
+          <ChevronRight size={30} />
+        </button>
+      </div>
     </div>
   );
 }
