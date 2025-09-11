8055ca45167724609c8a68f7d3757a1e0e5bc002
risinger7 <viktorrisinger@gmail.com>
2025-09-11 19:01:38 +0200

    fix

--- src/components/fonts/fonts.ts
+++ src/components/fonts/fonts.ts
@@ -1,9 +1,8 @@
-import { Playwrite_DE_Grund, Lexend_Deca } from "next/font/google";
-/* import localFont from "next/font/local"; */
+import { Lexend_Deca } from "next/font/google";
 
 //playwrite
-const playwrite200 = Playwrite_DE_Grund({ weight: "200" });
-const playwrite400 = Playwrite_DE_Grund({ weight: "400" });
+/* const playwrite200 = Playwrite_DE_Grund({ weight: "200" });
+const playwrite400 = Playwrite_DE_Grund({ weight: "400" }); */
 //lexend
 const lexend200 = Lexend_Deca({ subsets: ["latin"], weight: "200" });
 const lexend300 = Lexend_Deca({ subsets: ["latin"], weight: "300" });
@@ -11,8 +10,8 @@ const lexend400 = Lexend_Deca({ subsets: ["latin"], weight: "400" });
 const lexend700 = Lexend_Deca({ subsets: ["latin"], weight: "700" });
 
 export {
-  playwrite200,
-  playwrite400,
+  /*   playwrite200,
+  playwrite400, */
   lexend200,
   lexend300,
   lexend400,
